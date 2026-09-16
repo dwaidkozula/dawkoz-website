f55de6583b51e0370e15c4b9ac73ba88a793d200
Dawid Kozula <dawid.kozula10@gmail.com>
2026-09-17 01:27:07 +0200

    Load trust metrics on DAWKOZ site

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ http.createServer((req,res)=>{
   fs.readFile(file,(err,data)=>{
     if(err){res.writeHead(404,{'Content-Type':'text/plain; charset=utf-8','Cache-Control':'no-store'});return res.end('404');}
     let output=data;
-    if(urlPath==='/index.html') output=Buffer.from(data.toString('utf8').replace('</body>','<script src="/pricing.js?v=20260916-3"></script></body>'));
+    if(urlPath==='/index.html') output=Buffer.from(data.toString('utf8').replace('</body>','<script src="/pricing.js?v=20260916-3"></script><script src="/trust.js?v=20260917-1"></script></body>'));
     res.writeHead(200,{
       'Content-Type':types[path.extname(file)]||'application/octet-stream',
       'Cache-Control':'no-store, no-cache, must-revalidate, proxy-revalidate',
